43504caa232ba40feae4b290f72458cca59ee1b1
Giovanni Aratico <giovanni.aratico@pgm-corp.com>
2018-10-19 13:30:47 -0700

    Changed folder structure

---
+++ public/games/index.js
@@ -0,0 +1,19 @@
+// 
+console.log('Loaded')
+// function something(text) {
+//     var newDiv = document.createElement("div");
+//     var newContent = document.createTextNode(text); 
+//     newDiv.appendChild(newContent);  
+//     var currentDiv = document.getElementById("consoleForm"); 
+//     document.body.appendChild(newDiv, currentDiv);
+// }
+
+// function alertFunction() {
+//     let id = document.getElementById('getConsole').value
+//     fetch('/console/' + id)
+//     .then( (data) => { return data.json() })
+//     .then(function(myJson) {
+//         something(JSON.stringify(myJson));
+//     })
+//     .catch( (error) => { console.log(error) })
+// }
